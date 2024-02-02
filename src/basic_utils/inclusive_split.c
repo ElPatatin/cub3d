7aee346b5e0e2e0ddea5a3a33e5d898eb6d7453d
Cristóbal Peset Cruella <43825378+ElPatatin@users.noreply.github.com>
2024-02-03 00:09:10 +0100

    started doing the inclusive split

--- src/basic_utils/inclusive_split.c
+++ src/basic_utils/inclusive_split.c
@@ -1,21 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_spc.c                                           :+:      :+:    :+:   */
+/*   inclusive_split.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cpeset-c <cpeset-c@student.42barce.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/02 11:56:15 by alajara-          #+#    #+#             */
-/*   Updated: 2024/02/03 00:01:32 by cpeset-c         ###   ########.fr       */
+/*   Created: 2024/02/02 23:59:24 by cpeset-c          #+#    #+#             */
+/*   Updated: 2024/02/03 00:08:49 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "cub3d_basic_utils.h"
 
-t_bool	is_spc(char c)
+char    **inclusive_split(char const *str, char chr)
 {
-	if (c == ' ' || c == '\t')
-		return (TRUE);
-	return (FALSE);
+    char    **tab;
+
+    if (!str)
+        return (NULL);
+    tab = (char **)ft_calloc((ft_strlen(str) + 1), sizeof(char *));
+    if (!tab)
+        return (NULL);
+
+    return (tab);
 }
