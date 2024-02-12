c071cfb8403398f6332891ac5ed468b37d2ece33
ElPatatin <cristobalpes1@gmail.com>
2024-02-12 19:15:50 +0100

    caca

--- inc/cub3d_render_private.h
+++ inc/cub3d_render_private.h
@@ -6,7 +6,7 @@
 /*   By: cpeset-c <cpeset-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 12:17:07 by cpeset-c          #+#    #+#             */
-/*   Updated: 2024/02/12 12:18:31 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2024/02/12 15:49:43 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define CUB3D_RENDER_PRIVATE_H
 
 # include "cub3d_struct.h"
+
+# define RAYS 64
 
 static void	put_background_layer(t_graphics *graphics);
 
