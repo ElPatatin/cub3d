2341b6c672559feed8a91e89e964ba7ace9987b0
ElPatatin <cristobalpes1@gmail.com>
2024-02-18 20:08:22 +0100

    working texture files

--- src/textures/texture_init.c
+++ src/textures/texture_init.c
@@ -6,7 +6,7 @@
 /*   By: cpeset-c <cpeset-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 18:43:31 by cpeset-c          #+#    #+#             */
-/*   Updated: 2024/02/18 16:04:40 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2024/02/18 20:07:32 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,23 @@ void	texture_init(t_graphics *g)
 	int	y;
 
 	x = -1;
-	g->tex.texture[0] = (uint32_t *)ft_calloc(sizeof(uint32_t), TEXWIDTH * TEXHEIGHT);
-	g->tex.texture[1] = (uint32_t *)ft_calloc(sizeof(uint32_t), TEXWIDTH * TEXHEIGHT);
 	g->tex.texture[2] = (uint32_t *)ft_calloc(sizeof(uint32_t), TEXWIDTH * TEXHEIGHT);
 	g->tex.texture[3] = (uint32_t *)ft_calloc(sizeof(uint32_t), TEXWIDTH * TEXHEIGHT);
+	t_image *img1;
+	img1 = malloc(sizeof(t_image));
+	img1 = mlx_xpm_file_to_image(g->mlx, g->info->txr_no, &img1->width, &img1->height);
+	t_image *img2;
+	img2 = malloc(sizeof(t_image));
+	img2 = mlx_xpm_file_to_image(g->mlx, g->info->txr_so, &img2->width, &img2->height);
 	while (++x < TEXWIDTH)
 	{
 		y = -1;
 		while (++y < TEXHEIGHT)
 		{
-			g->tex.texture[0][TEXWIDTH * y + x] = 0x00FF0000; //rojo sud
-			g->tex.texture[1][TEXWIDTH * y + x] = 0x0000FF00; //verde norte
-			g->tex.texture[2][TEXWIDTH * y + x] = 0x000000FF; //azul este
-			if ((x < TEXWIDTH / 2 && y < TEXHEIGHT / 2)       //	o esta
+			g->tex.texture[0] = (uint32_t *)mlx_get_data_addr(img1, &img1->bpp, &img1->line_len, &img1->endian);
+			g->tex.texture[1] = (uint32_t *)mlx_get_data_addr(img2, &img2->bpp, &img2->line_len, &img2->endian);
+			g->tex.texture[2][TEXWIDTH * y + x] = 0x000000FF;
+			if ((x < TEXWIDTH / 2 && y < TEXHEIGHT / 2)
 				|| (x >= TEXWIDTH / 2 && y >= TEXHEIGHT / 2))
 				g->tex.texture[3][TEXWIDTH * y + x] = 0x01000000;
 			else
